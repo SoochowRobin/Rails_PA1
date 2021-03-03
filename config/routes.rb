5376ec10bd0d803f20b3db15ca96fdb48a4d3541
Robin_Chen <sudacgb@gmail.com>
2021-03-03 14:19:52 -0500

    add tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :instructors
   resources :courses
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to:"instructors#index"
+  root to:"home#index"
 end
